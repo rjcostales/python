379fb2ac3564928d177abbdbcc5fd143ee6e1942
jcostales <jesse.costales@sigfig.com>
2017-06-23 11:40:09 -0700

    rename file

--- request/finance.py
+++ request/finance.py
@@ -4,7 +4,7 @@ url = 'http://www.google.com/finance/info'
 
 params = {'q': 'AAPL'}
 
-response = requests.get(url, params,)
+response = requests.get(url, params, )
 
 print(response.content.decode('utf-8'))
 
@@ -12,12 +12,12 @@ url = 'http://www.google.com/finance/historical'
 
 params = {
     'q': 'GOOG',
-    'startdate':'01-Jan-16',
-    'enddate':'31-Dec-16',
-    'output':'csv'
+    'startdate': '01-Jan-16',
+    'enddate': '31-Dec-16',
+    'output': 'csv'
 }
 
-response = requests.get(url, params,)
+response = requests.get(url, params, )
 
 data = response.content.decode('utf-8')
 
