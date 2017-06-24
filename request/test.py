ab13d5ffec55851d6c962b80adca8a1843a3323d
jesse <rjcostales@gmail.com>
2017-06-24 10:23:01 -0700

    refactor

--- request/test.py
+++ request/test.py
@@ -3,24 +3,25 @@ import requests
 ## info
 url = 'http://www.google.com/finance/info'
 
-params = {'q': 'AAPL'}
+params = {'q': 'AAPL,GOOG'}
 
-response = requests.get(url, params,)
+response = requests.get(url, params)
 
-print(response.content.decode('utf-8'))
+data = response.content.decode('utf-8')[4:]
 
+print(data)
 
 ## historical
 url = 'http://www.google.com/finance/historical'
 
 params = {
     'q': 'GOOG',
-    'startdate':'01-Jan-16',
-    'enddate':'31-Dec-16',
-    'output':'csv'
+    'startdate': '01-Jan-16',
+    'enddate': '31-Dec-16',
+    'output': 'csv'
 }
 
-response = requests.get(url, params,)
+response = requests.get(url, params)
 
 data = response.content.decode('utf-8')
 print(data)
