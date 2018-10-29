455b079a7be90ee930e7d437510f403eb9abf53f
jesse <rjcostales@gmail.com>
2018-10-29 12:34:32 -0700

    refactor from codex

--- http/giphy.py
+++ http/giphy.py
@@ -1,5 +1,4 @@
 import json
-
 import requests
 
 API_KEY = "20ETF5kMibhng2nxxVe5G01NKBx3XUO8"
@@ -9,8 +8,7 @@ response = requests.get("http://api.giphy.com/v1/gifs/search?api_key=%s&q=%s" % 
 print(response.status_code)
 
 text = json.loads(response.text)
-
 data = text["data"]
 
 for dat in data:
-    print(json.dumps(dat["images"]["preview"]))
+    print(json.dumps(dat["title"]), json.dumps(dat["images"]["preview"]["mp4"]))
