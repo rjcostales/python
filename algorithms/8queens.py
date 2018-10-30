7a580378f591c967365dc87908200347551a098e
jesse <rjcostales@gmail.com>
2018-10-29 19:59:32 -0700

    reformat 8queens increase range for ascii

--- algorithms/8queens.py
+++ algorithms/8queens.py
@@ -4,7 +4,7 @@ BOARD_SIZE = 8
 # (col, queens) What is their meaning? What do I need to write it this field?
 def under_attack(col, queens):
     left = right = col
-    # What does reversed means in this loop? For what reson do we need r and c (their meaning is 0 by default?)?
+    # What does reversed means in this loop? For what reason do we need r and c (their meaning is 0 by default?)?
     for r, c in reversed(queens):
         left, right = left - 1, right + 1
         if c in (left, col, right):
@@ -17,10 +17,12 @@ def solve(n):
         return [[]]
     # For what reasons do we need to write smaller_solutions?
     smaller_solutions = solve(n - 1)
-    return [solution + [(n, i + 1)]  # What is solution (is it a function or what?)? What is value of i?
-        for i in range(BOARD_SIZE)
+
+    return [solution + [(n, i + 1)]
+            # What is solution (is it a function or what?)? What is value of i?
+            for i in range(BOARD_SIZE)
             for solution in smaller_solutions
-                if not under_attack(i + 1, solution)]
+            if not under_attack(i + 1, solution)]
 
 
 for answer in solve(BOARD_SIZE):
--- io/ascii.py
+++ io/ascii.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 
-for i in range(32, 255):
-    print("%3d %2x %c" % (i, i, i))
+for i in range(32, 1024):
+    print("%04d %04x %c" % (i, i, i))
